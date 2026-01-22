6b0d0af4555a81ce25b2ae4c08f8eface3a62167
SaniolJR <mateusz.sadowski04@wp.pl>
2026-01-22 06:17:26 +0100

    BE: made TextFilter in GetDrinksDTO unnecesary

--- backend/CA_Application/DTOs/GetDrinksDTO.cs
+++ backend/CA_Application/DTOs/GetDrinksDTO.cs
@@ -4,7 +4,7 @@ namespace CA_Application.DTOs
     {
 
         public bool Verified { get; set; } = false;   //can in URL be as true, default is false
-        public string TextFilter { get; set; } = string.Empty;
+        public string? TextFilter { get; set; } = string.Empty;
         public int MissingIngredients { get; set; } = 0;
         public List<string> Ingredients { get; set; } = new List<string>();
         public int PageSize { get; set; } = 20;
